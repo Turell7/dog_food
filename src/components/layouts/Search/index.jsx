6512a4d12890736232f3b549f447ecc15bfb2613
Н С <nikolai3578@gmail.com>
2023-01-31 18:12:16 +0300

    Added query params

--- src/components/layouts/Search/index.jsx
+++ src/components/layouts/Search/index.jsx
@@ -6,18 +6,35 @@ import { setSearch } from '../../../redux/slices/searchProductsSlice/searchProdu
 
 export function Search() {
   const [searchParams, setSearchParams] = useSearchParams()
-  const [input, setInput] = useState(() => searchParams.get('q') ?? '')
+  const [input, setInput] = useState(() => searchParams.get('search') ?? '')
   const dispatch = useDispatch()
   const setsearchState = (value) => dispatch(setSearch(value))
   const debounceValue = useDebounce(input, 350)
 
   useEffect(() => {
-    setSearchParams({ q: input })
+    setSearchParams({
+      ...Object.fromEntries(searchParams.entries()),
+      search: input,
+    })
   }, [input])
 
   useEffect(() => {
     setsearchState(debounceValue)
   }, [debounceValue])
+
+  const searchHandler = (e) => {
+    setInput(e.target.value)
+
+    if (!e.target.value) {
+      searchParams.delete('search')
+      setSearchParams(searchParams)
+    } else {
+      setSearchParams({
+        ...Object.fromEntries(searchParams.entries()),
+        search: e.target.value,
+      })
+    }
+  }
 
   return (
     <div className="form-control">
@@ -25,7 +42,7 @@ export function Search() {
         type="text"
         placeholder="Search"
         className="input input-bordered"
-        onChange={(e) => setInput(e.target.value)}
+        onChange={searchHandler}
       />
     </div>
   )
--- src/components/layouts/SortProductsBar/index.jsx
+++ src/components/layouts/SortProductsBar/index.jsx
@@ -1,18 +1,35 @@
+import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { useSearchParams } from 'react-router-dom'
 import { setSort, sortValues } from '../../../redux/slices/sortProductsSlice/sortProductsSlice'
 
 export function SortProductsBar() {
   const sortValue = useSelector((store) => store.sort.value)
   const dispatch = useDispatch()
+  const [searchParams, setSearchParams] = useSearchParams({ sort: sortValue })
+
+  useEffect(() => {
+    if (searchParams.get('sort')) {
+      dispatch(setSort(searchParams.get('sort')))
+    }
+  }, [])
+
+  const changeSortHandler = (value) => {
+    setSearchParams({
+      ...Object.fromEntries(searchParams.entries()),
+      sort: value,
+    })
+    dispatch(setSort(value))
+  }
 
   return (
     <div className="navbar bg-base-100">
-      <button className={`btn btn-ghost btn-sm ${sortValue === sortValues.POPULAR && 'btn-active'}`} onClick={() => dispatch(setSort(sortValues.POPULAR))} type="button">Popular</button>
-      <button className={`btn btn-ghost btn-sm ${sortValue === sortValues.NEWEST && 'btn-active'}`} onClick={() => dispatch(setSort(sortValues.NEWEST))} type="button">Newest</button>
-      <button className={`btn btn-ghost btn-sm ${sortValue === sortValues.PRICE_LOW && 'btn-active'}`} onClick={() => dispatch(setSort(sortValues.PRICE_LOW))} type="button">Low price</button>
-      <button className={`btn btn-ghost btn-sm ${sortValue === sortValues.PRICE_HIGH && 'btn-active'}`} onClick={() => dispatch(setSort(sortValues.PRICE_HIGH))} type="button">High price</button>
-      <button className={`btn btn-ghost btn-sm ${sortValue === sortValues.RATE && 'btn-active'}`} onClick={() => dispatch(setSort(sortValues.RATE))} type="button">Rate</button>
-      <button className={`btn btn-ghost btn-sm ${sortValue === sortValues.DISCOUNT && 'btn-active'}`} onClick={() => dispatch(setSort(sortValues.DISCOUNT))} type="button">Discount</button>
+      <button className={`btn btn-ghost btn-sm ${sortValue === sortValues.POPULAR && 'btn-active'}`} onClick={() => changeSortHandler(sortValues.POPULAR)} type="button">Popular</button>
+      <button className={`btn btn-ghost btn-sm ${sortValue === sortValues.NEWEST && 'btn-active'}`} onClick={() => changeSortHandler(sortValues.NEWEST)} type="button">Newest</button>
+      <button className={`btn btn-ghost btn-sm ${sortValue === sortValues.PRICE_LOW && 'btn-active'}`} onClick={() => changeSortHandler(sortValues.PRICE_LOW)} type="button">Low price</button>
+      <button className={`btn btn-ghost btn-sm ${sortValue === sortValues.PRICE_HIGH && 'btn-active'}`} onClick={() => changeSortHandler(sortValues.PRICE_HIGH)} type="button">High price</button>
+      <button className={`btn btn-ghost btn-sm ${sortValue === sortValues.RATE && 'btn-active'}`} onClick={() => changeSortHandler(sortValues.RATE)} type="button">Rate</button>
+      <button className={`btn btn-ghost btn-sm ${sortValue === sortValues.DISCOUNT && 'btn-active'}`} onClick={() => changeSortHandler(sortValues.DISCOUNT)} type="button">Discount</button>
     </div>
   )
 }
